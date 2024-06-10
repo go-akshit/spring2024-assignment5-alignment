299bf69f7f17f3fa6d08baa074f7d59fb741335b
go-akshit <akshit@stanford.edu>
2024-06-09 19:03:25 -0700

    hh_try

---
+++ cs336_alignment/dpo.py
@@ -0,0 +1,52 @@
+import torch
+import gzip
+import json
+
+def hh_dataset(file_paths, output_file_path):
+    dataset = []
+    for file_path in file_paths:
+        category = file_path.split('/')[-2]
+        with gzip.open(file_path, 'rt', 'utf-8') as f:
+            for line in f:
+                example = json.loads(line)
+                chosen = example['chosen']
+                reject = example['rejected']
+                if chosen.count('\n\nHuman') > 1:
+                    continue
+                else: 
+                    len_human_title = len('\n\nHuman: ')
+                    len_assistant_title = len('\n\nAssistant: ')
+                    
+                    chosen_human_index = chosen.index('\n\nHuman:')
+                    chosen_assistant_index = chosen.index('\n\nAssistant:')
+                    reject_human_index = reject.index('\n\nHuman:')
+                    reject_assistant_index = reject.index('\n\nAssistant:')
+    
+                    human = chosen[chosen_human_index + len_human_title:chosen_assistant_index]
+                    
+                    chosen_assistant = chosen[chosen_assistant_index + len_assistant_title:]
+                    reject_assistant = reject[reject_assistant_index + len_assistant_title:]
+                    entry = {}
+                    entry['instruction'] = human
+                    entry['chosen'] = chosen_assistant
+                    entry['rejected'] = reject_assistant
+                    entry['category'] = category
+                    dataset.append(entry)
+    
+    with open(output_file_path, 'w') as f:
+        for entry in dataset:
+            f.write(json.dumps(entry) + '\n')
+
+                    
+
+def main():
+    input_file_paths = ['/home/shared/hh/helpful-base.jsonl.gz',
+                        '/home/shared/hh/helpful-rejection-sampled.jsonl.gz',
+                        '/home/shared/hh/helpful-online.jsonl.gz',
+                        '/home/shared/hh/harmless-base.jsonl.gz']
+    output_file_path = 'hh_dataset.jsonl'
+    hh_dataset(input_file_paths, output_file_path)  
+
+if __name__ == '__main__':
+    main()
+
