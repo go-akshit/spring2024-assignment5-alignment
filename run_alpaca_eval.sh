d547ccf31b76e1c04703ffcc3b667ed81c39f363
go-akshit <akshit@stanford.edu>
2024-06-10 18:57:04 -0700

    try

--- run_alpaca_eval.sh
+++ run_alpaca_eval.sh
@@ -8,4 +8,4 @@
 #SBATCH --gpus=2
 #SBATCH --output=alpaca_eval_%j.out
 #SBATCH --error=alpaca_eval_%j.err
-alpaca_eval --model_outputs 'alpaca_eval_predictions.json' --annotators_config 'scripts/alpaca_eval_vllm_llama3_70b_fn' --base-dir '.'
+alpaca_eval --model_outputs 'alpaca_eval_predictions_sft.json' --annotators_config 'scripts/alpaca_eval_vllm_llama3_70b_fn' --base-dir '.'
